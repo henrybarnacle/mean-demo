b8cbc64c54af29716081db45ce7e6523da0f162b
Henry Barnacle <henrybarnacle@yahoo.com>
2017-10-31 15:33:52 -0400

    intial commit

---
+++ client/js/controllers/meetups-controller.js
@@ -0,0 +1,25 @@
+
+//var meetupsApp = angular.module('meetupsApp',[]);
+
+app.controller('meetupsController', ['$scope', '$resource', 
+	function($scope, $resource) {
+		var Meetup = $resource('/api/meetups');
+  
+  Meetup.query(function(results) {
+  	$scope.meetups = results;
+  });
+
+  $scope.meetups=[];
+  
+
+  $scope.createMeetup = function() {
+  	var meetup = new Meetup();
+  	meetup.name = $scope.meetupName;
+  	meetup.$save(function(result) {
+  		$scope.meetups.push(result);
+  		$scope.meetupName = '';
+  	});
+  	//$scope.meetups.push({ name: $scope.meetupName});
+  	//$scope.meetupName = '';
+  }
+}]);
